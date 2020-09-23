13c023bf48aabca47da84f93cb4ac51e11820df6
leogiraldimg <leogiraldimg@hotmail.com>
2020-09-22 22:20:05 -0300

    Implementing end-to-end tests

---
+++ backend/src/resetDb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const conStr = 'mongodb://db:27017/footapp_test'
+const User = require('../src/api/user/user')
+
+mongoose.connect(conStr, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+
+    User.deleteMany({}, (err) => {
+        mongoose.disconnect()
+    })
+
+})
+
--- frontend/cypress/integration/auth_spec.js
+++ frontend/cypress/integration/auth_spec.js
@@ -1,9 +1,230 @@
 describe('Auth component tests', () => {
-    it('renders successfully - login mode', () => {
-        
+    beforeEach(async () => {
+        await cy.exec('docker exec footapp-backend-test npm run db:reset')
     })
 
-    it('renders successfully - signupmode', () => {
-        
+    it('renders successfully - login mode', () => {
+        cy.visit('/')
+        cy.get('[data-test-id="email"]')
+        cy.get('[data-test-id="password"]')
+        cy.get('.auth-login-mode')
+        cy.get('.auth-btn')
+    })
+
+    it('renders successfully - signup mode', () => {
+        cy.visit('/')
+        cy.get('[data-test-id="email"]')
+        cy.get('[data-test-id="password"]')
+        cy.get('.auth-login-mode')
+        cy.get('.auth-btn')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]')
+        cy.get('[data-test-id="email"]')
+        cy.get('[data-test-id="password"]')
+        cy.get('[data-test-id="confirm_password"]')
+    })
+
+    it('signup successfully', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('[data-test-id="confirm_password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(JSON.parse(localStorage.getItem('_footapp')).email).to.eq('foo@foo.com.br')
+        })
+    })
+
+    it('login successfully', () => {
+        cy.request('POST', 'http://localhost:3003/oapi/user/signup', { 
+            name: 'Foo Foolish',
+            email: 'foo@foo.com.br', 
+            password: 'Foo@123!',
+            confirm_password: 'Foo@123!'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.visit('/')
+            cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+            cy.get('[data-test-id="password"]').type('Foo@123!')
+            cy.get('.auth-btn').click().should(() => {
+                expect(JSON.parse(localStorage.getItem('_footapp')).email).to.eq('foo@foo.com.br')
+            })
+        })
+    })
+
+    it('signup fail - no name', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('[data-test-id="confirm_password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - no email', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('[data-test-id="confirm_password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - no password', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="confirm_password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - no confirm_password', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - wrong e-mail', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="email"]').type('foo')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('[data-test-id="confirm_password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - wrong password', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="password"]').type('Foo')
+        cy.get('[data-test-id="confirm_password"]').type('Foo')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - wrong confirm_password', () => {
+        cy.visit('/')
+        cy.get('.auth-login-mode').click()
+        cy.get('[data-test-id="name"]').type('Foo Foolish')
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('[data-test-id="confirm_password"]').type('Foo@123')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('signup fail - user already registered', () => {
+        cy.request('POST', 'http://localhost:3003/oapi/user/signup', { 
+            name: 'Foo Foolish',
+            email: 'foo@foo.com.br', 
+            password: 'Foo@123!',
+            confirm_password: 'Foo@123!'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.visit('/')
+            cy.get('.auth-login-mode').click()
+            cy.get('[data-test-id="name"]').type('Foo Foolish')
+            cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+            cy.get('[data-test-id="password"]').type('Foo@123!')
+            cy.get('[data-test-id="confirm_password"]').type('Foo@123!')
+            cy.get('.auth-btn').click().should(() => {
+                expect(localStorage.getItem('_footapp')).to.eq(null)
+            })
+        })
+    })
+
+    it('login fail - no user registered', () => {
+        cy.visit('/')
+        cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+        cy.get('[data-test-id="password"]').type('Foo@123!')
+        cy.get('.auth-btn').click().should(() => {
+            expect(localStorage.getItem('_footapp')).to.eq(null)
+        })
+    })
+
+    it('login fail - no email', () => {
+        cy.request('POST', 'http://localhost:3003/oapi/user/signup', { 
+            name: 'Foo Foolish',
+            email: 'foo@foo.com.br', 
+            password: 'Foo@123!',
+            confirm_password: 'Foo@123!'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.visit('/')
+            cy.get('[data-test-id="password"]').type('Foo@123!')
+            cy.get('.auth-btn').click().should(() => {
+                expect(localStorage.getItem('_footapp')).to.eq(null)
+            })
+        })      
+    })
+
+    it('login fail - no password', () => {
+        cy.request('POST', 'http://localhost:3003/oapi/user/signup', { 
+            name: 'Foo Foolish',
+            email: 'foo@foo.com.br', 
+            password: 'Foo@123!',
+            confirm_password: 'Foo@123!'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.visit('/')
+            cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+            cy.get('.auth-btn').click().should(() => {
+                expect(localStorage.getItem('_footapp')).to.eq(null)
+            })
+        })      
+    })
+
+    it('login fail - wrong email', () => {
+        cy.request('POST', 'http://localhost:3003/oapi/user/signup', { 
+            name: 'Foo Foolish',
+            email: 'foo@foo.com.br', 
+            password: 'Foo@123!',
+            confirm_password: 'Foo@123!'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.visit('/')
+            cy.get('[data-test-id="email"]').type('foo@foo')
+            cy.get('[data-test-id="password"]').type('Foo@123!')
+            cy.get('.auth-btn').click().should(() => {
+                expect(localStorage.getItem('_footapp')).to.eq(null)
+            })
+        })
+    })
+
+    it('login fail - wrong password', () => {
+        cy.request('POST', 'http://localhost:3003/oapi/user/signup', { 
+            name: 'Foo Foolish',
+            email: 'foo@foo.com.br', 
+            password: 'Foo@123!',
+            confirm_password: 'Foo@123!'
+        }).then((response) => {
+            expect(response.status).to.eq(200)
+            cy.visit('/')
+            cy.get('[data-test-id="email"]').type('foo@foo.com.br')
+            cy.get('[data-test-id="password"]').type('Foo@123')
+            cy.get('.auth-btn').click().should(() => {
+                expect(localStorage.getItem('_footapp')).to.eq(null)
+            })
+        })
     })
 })
