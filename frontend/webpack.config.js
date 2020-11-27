a14fe0f6c4c73cf38e755c6f125b7f19b012ca6e
Leonardo Giraldi <leogiraldimg@gmail.com>
2020-11-26 23:45:49 -0300

    Setting process.env.BACKEND_URL

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -27,7 +27,8 @@ module.exports = {
         }),
         new ExtractTextPlugin('app.css'),
         new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL)
         })
     ],
     module: {
